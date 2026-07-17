75b3f7a2598083437acd3c6055a7e653555c6929
Sivuyile Mtwetwe <sivuyilemtwetwe@gmail.com>
2026-07-17 09:35:16 +0200

    Created solutiom for encapsulation.  Added Transaction and BankAccount as examples and Added Transactions to BankAccount

---
+++ ObjectOrientationClassExercises/Encapsulation/AccountType.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ObjectOrientationClassExercises.Encapsulation
+{
+    internal enum AccountType
+    {
+        Savings,
+        Cheque,
+        Credit,
+        Current
+    }
+}
---
+++ ObjectOrientationClassExercises/Encapsulation/Transaction.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ObjectOrientationClassExercises.Encapsulation
+{
+    internal class Transaction
+    {
+        public DateTime TransactionDateTime { get; set; }
+        public decimal Amount { get; set; }
+        public int MyProperty { get; set; }
+
+        public string Description { get; set; }
+
+        public string Reference { get; set; }
+
+        public TransactionType TransactionType { get; set; }
+
+
+    }
+}
